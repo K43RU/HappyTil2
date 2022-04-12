7e59956431710a9d473c95f2ff5f02f1ee7f7026
Your Name <>
2022-04-11 21:54:31 -0300

    ok

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -7,7 +7,24 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { FormsModule } from '@angular/forms';
 import CheckLogged from './checkLogged.canactivate';
 import { HomeModule } from './home/home.module';
-import { RegistroComponent } from './registro/registro.component'
+import { RegistroComponent } from './registro/registro.component';
+import {
+  SocialLoginModule,
+  AuthServiceConfig,
+  GoogleLoginProvider
+} from "angular-6-social-login-v2";
+
+export function getAuthServiceConfigs() {
+  let config = new AuthServiceConfig(
+      [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider("402231336047-9jsgeagvcoa8fqb6eled83fj2hnodt5d.apps.googleusercontent.com")
+        }
+      ]
+  );
+  return config;
+}
 
 @NgModule({
   declarations: [
@@ -24,9 +41,14 @@ import { RegistroComponent } from './registro/registro.component'
     RouterModule,
     CommonModule,
     FormsModule,
-    HomeModule
+    HomeModule,
+    SocialLoginModule
   ],
-  providers: [ CheckLogged ],
+  providers: [ CheckLogged,
+    {
+    provide: AuthServiceConfig,
+    useFactory: getAuthServiceConfigs
+    } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
